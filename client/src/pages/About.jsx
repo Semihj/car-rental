494f57bc099dd49fe9818b4038324640dab24e92
Semih <shouldahjdh@gmail.com>
2023-12-31 14:34:17 +0300

    I swear its the last one

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,10 @@
 
 export default function About() {
   return (
-    <div className="flex flex-col  h-screen items-center m-20 " >
-      <div className="w-full h-full max-h-[600px] max-w-lg items-center flex flex-col border-2 mx-auto shadow-lg p-5 gap-10">
-      <h1 className="text-[44px] mt-10 font-semibold " >About Us</h1>
-      <p>
+    <div className="flex flex-col w-screen mx-auto p-5  h-screen items-center m-20 " >
+      <div className="min-h-max w-full   items-center flex flex-col items-center justify-center border-2 mx-auto shadow-lg p-5 gap-10">
+      <h1 className="text-[44px] mt-10 font-semibold p-7" >About Us</h1>
+      <p className="" >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, molestias debitis facilis ratione dolores est aperiam exercitationem necessitatibus iusto magni.
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde exercitationem quia ad pariatur ipsa? Dignissimos maxime sed libero odit cupiditate voluptatum, vitae ex necessitatibus placeat quae atque, provident est, sunt nemo. Excepturi ducimus voluptas non facere minima iure, beatae fuga aliquam necessitatibus? Debitis natus fuga facilis iure. Deleniti iste perspiciatis minima quia possimus? Porro accusantium obcaecati ullam, ipsa ad repellat quod, nam voluptatem amet consequatur sit aperiam? Delectus, quis omnis!
       </p>
